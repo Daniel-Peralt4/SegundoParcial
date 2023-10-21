2f1c99523e868dd84130def1a2b0a2136bf96690
Daniel-Peralt4 <dperaltapenuela07@gmail.com>
2023-10-21 18:53:43 -0500

    inicio del parcial

---
+++ Entidad/Empleado.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    [Serializable]
+    public class Empleado
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public double SalarioBase { get; set;}
+        public string Estado { get; set; }
+
+        public Empleado() { }
+        public Empleado(int id, string nombre, double salarioBase, string estado)
+        {
+            ID = id;
+            Nombre = nombre;
+            SalarioBase = salarioBase;
+            Estado = estado;
+        }
+        public override string ToString()
+        {
+            return $"{ID};{Nombre};{SalarioBase};{Estado}";
+        }
+        public void RegistrarEmpleado(int id, string nombre, double salariobase, string estado)
+        {
+            Empleado nuevoEmpleado = new Empleado(id, nombre, salariobase, estado);
+        }
+    }
+}
